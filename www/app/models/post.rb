1f6ce1a22893c2691f0571b882fcc6e9deb4e5c1
Jon Gillies <jon@gillies.us>
2013-04-10 19:53:42 -0700

    .

--- www/app/models/post.rb
+++
@@ -1,3 +0,0 @@
-class Post < ActiveRecord::Base
-  attr_accessible :description, :title
-end
--- www/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rortb::Application.config.secret_token = 'bb3c1a1e82062ca6ae7bac84204740c970b7caf57ea33398905cac73b0a21f1f744dd5580195174b4545894a84f14c2288f48e9b993502e87977b11f31d2f503'
