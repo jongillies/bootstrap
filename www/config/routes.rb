d229daf9fbbcc2e537eb1b7c0699a9bdce096df9
Jon Gillies <jon@gillies.us>
2013-04-11 09:02:39 -0700

    moved test database to sqlite, updated devise for a drepication warning in the routes file

--- www/config/routes.rb
+++ www/config/routes.rb
@@ -4,7 +4,8 @@ MyApplication::Application.routes.draw do
   end
   root :to => "home#index"
 
-  devise_for :users do
+  devise_for :users
+  devise_scope :users do
     get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
   end
 
