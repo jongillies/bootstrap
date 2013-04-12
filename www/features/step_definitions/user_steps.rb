f46ec1ab9480a7c395426072b34f134d24bb85f8
Jon Gillies <jon@gillies.us>
2013-04-11 18:58:31 -0700

    Converted to ruby 1.9 hash syntax

--- www/features/step_definitions/user_steps.rb
+++ www/features/step_definitions/user_steps.rb
@@ -1,12 +1,12 @@
 ### UTILITY METHODS ###
 
 def create_visitor
-  @visitor ||= { :name => "Testy McUserton", :email => "example@example.com",
-    :password => "changeme", :password_confirmation => "changeme" }
+  @visitor ||= {name:     "Testy McUserton", email: "example@example.com",
+                password: "changeme", password_confirmation: "changeme"}
 end
 
 def find_user
-  @user ||= User.where(:email => @visitor[:email]).first
+  @user ||= User.where(email: @visitor[:email]).first
 end
 
 def create_unconfirmed_user
@@ -23,25 +23,25 @@ def create_user
 end
 
 def delete_user
-  @user ||= User.where(:email => @visitor[:email]).first
+  @user ||= User.where(email: @visitor[:email]).first
   @user.destroy unless @user.nil?
 end
 
 def sign_up
   delete_user
   visit '/users/sign_up'
-  fill_in "user_name", :with => @visitor[:name]
-  fill_in "user_email", :with => @visitor[:email]
-  fill_in "user_password", :with => @visitor[:password]
-  fill_in "user_password_confirmation", :with => @visitor[:password_confirmation]
+  fill_in "user_name", with: @visitor[:name]
+  fill_in "user_email", with: @visitor[:email]
+  fill_in "user_password", with: @visitor[:password]
+  fill_in "user_password_confirmation", with: @visitor[:password_confirmation]
   click_button "Sign up"
   find_user
 end
 
 def sign_in
   visit '/users/sign_in'
-  fill_in "user_email", :with => @visitor[:email]
-  fill_in "user_password", :with => @visitor[:password]
+  fill_in "user_email", with: @visitor[:email]
+  fill_in "user_password", with: @visitor[:password]
   click_button "Sign in"
 end
 
@@ -85,38 +85,38 @@ end
 
 When /^I sign up with an invalid email$/ do
   create_visitor
-  @visitor = @visitor.merge(:email => "notanemail")
+  @visitor = @visitor.merge(email: "notanemail")
   sign_up
 end
 
 When /^I sign up without a password confirmation$/ do
   create_visitor
-  @visitor = @visitor.merge(:password_confirmation => "")
+  @visitor = @visitor.merge(password_confirmation: "")
   sign_up
 end
 
 When /^I sign up without a password$/ do
   create_visitor
-  @visitor = @visitor.merge(:password => "")
+  @visitor = @visitor.merge(password: "")
   sign_up
 end
 
 When /^I sign up with a to short password$/ do
   create_visitor
-  @visitor = @visitor.merge(:password => "short", :password_confirmation => "short")
+  @visitor = @visitor.merge(password: "short", password_confirmation: "short")
   sign_up
 end
 
 When $/ do
   create_visitor
-  @visitor = @visitor.merge(:password => "")
+  @visitor = @visitor.merge(password: "")
   sign_up
 end
 
 
 When /^I sign up with a mismatched password confirmation$/ do
   create_visitor
-  @visitor = @visitor.merge(:password_confirmation => "changeme123")
+  @visitor = @visitor.merge(password_confirmation: "changeme123")
   sign_up
 end
 
@@ -125,19 +125,19 @@ When /^I return to the site$/ do
 end
 
 When /^I sign in with a wrong email$/ do
-  @visitor = @visitor.merge(:email => "wrong@example.com")
+  @visitor = @visitor.merge(email: "wrong@example.com")
   sign_in
 end
 
 When /^I sign in with a wrong password$/ do
-  @visitor = @visitor.merge(:password => "wrongpass")
+  @visitor = @visitor.merge(password: "wrongpass")
   sign_in
 end
 
 When /^I edit my account details$/ do
   click_link "Edit account"
-  fill_in "user_name", :with => "newname"
-  fill_in "user_current_password", :with => @visitor[:password]
+  fill_in "user_name", with: "newname"
+  fill_in "user_current_password", with: @visitor[:password]
   click_button "Update"
 end
 
--- www/spec/controllers/users_controller_spec.rb
+++ www/spec/controllers/users_controller_spec.rb
@@ -8,17 +8,17 @@ describe UsersController do
   end
 
   describe "GET 'show'" do
-    
+
     it "should be successful" do
-      get :show, :id => @user.id
+      get :show, id: @user.id
       response.should be_success
     end
-    
+
     it "should find the right user" do
-      get :show, :id => @user.id
+      get :show, id: @user.id
       assigns(:user).should == @user
     end
-    
+
   end
 
 end
