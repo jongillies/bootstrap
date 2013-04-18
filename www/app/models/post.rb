a63d52333e2b5026231402b33e63e90e2adb753e
Jon Gillies <jon@gillies.us>
2013-04-18 08:50:57 -0700

    added better post seed data

--- www/app/models/post.rb
+++ www/app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 
-  attr_accessible :description, :title, :user_id
+  attr_accessible :description, :title, :user_id, :created_at, :updated_at
 
   belongs_to :user
 
--- www/generate_seeds.rb
+++ www/generate_seeds.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'faker'
+require 'active_support/all'
 
 1000.times do |i|
 
@@ -8,6 +9,8 @@ require 'faker'
   puts "  title: #{Faker::Lorem.words(rand(5)+3).map(&:capitalize).join(' ')}"
   puts "  description: #{Faker::Lorem.paragraphs(rand(5)+3).join('\n')}"
   puts "  user_id: #{rand(10)+1}"
-
+  created = rand(2.years).ago
+  puts "  created_at: #{created}"
+  puts "  updated_at: #{created + rand(5).day + rand(5).hours}"
 
 end
