5cf12231e853b6ced0cca06f024a009cc9cbdf3d
Jon Gillies <jon@gillies.us>
2013-05-13 14:05:56 -0700

    Added tabbing example in posts#show, updated user_steps

--- www/features/step_definitions/user_steps.rb
+++ www/features/step_definitions/user_steps.rb
@@ -1,12 +1,12 @@
 ### UTILITY METHODS ###
 
 def create_visitor
-  @visitor ||= {name:     'Testy McUserton', email: 'example@example.com',
-                password: 'changeme', password_confirmation: 'changeme'}
+  @visitor ||= { :name => "Testy McUserton", :email => "example@example.com",
+    :password => "changeme", :password_confirmation => "changeme" }
 end
 
 def find_user
-  @user ||= User.where(email: @visitor[:email]).first
+  @user ||= User.where(:email => @visitor[:email]).first
 end
 
 def create_unconfirmed_user
@@ -23,26 +23,26 @@ def create_user
 end
 
 def delete_user
-  @user ||= User.where(email: @visitor[:email]).first
+  @user ||= User.where(:email => @visitor[:email]).first
   @user.destroy unless @user.nil?
 end
 
 def sign_up
   delete_user
   visit '/users/sign_up'
-  fill_in 'user_name', with: @visitor[:name]
-  fill_in 'user_email', with: @visitor[:email]
-  fill_in 'user_password', with: @visitor[:password]
-  fill_in 'user_password_confirmation', with: @visitor[:password_confirmation]
-  click_button 'Sign up'
+  fill_in "user_name", :with => @visitor[:name]
+  fill_in "user_email", :with => @visitor[:email]
+  fill_in "user_password", :with => @visitor[:password]
+  fill_in "user_password_confirmation", :with => @visitor[:password_confirmation]
+  click_button "Sign up"
   find_user
 end
 
 def sign_in
   visit '/users/sign_in'
-  fill_in 'user_email', with: @visitor[:email]
-  fill_in 'user_password', with: @visitor[:password]
-  click_button 'Sign in'
+  fill_in "user_email", :with => @visitor[:email]
+  fill_in "user_password", :with => @visitor[:password]
+  click_button "Sign in"
 end
 
 ### GIVEN ###
@@ -85,38 +85,38 @@ end
 
 When /^I sign up with an invalid email$/ do
   create_visitor
-  @visitor = @visitor.merge(email: 'notanemail')
+  @visitor = @visitor.merge(:email => "notanemail")
   sign_up
 end
 
 When /^I sign up without a password confirmation$/ do
   create_visitor
-  @visitor = @visitor.merge(password_confirmation: '')
+  @visitor = @visitor.merge(:password_confirmation => "")
   sign_up
 end
 
 When /^I sign up without a password$/ do
   create_visitor
-  @visitor = @visitor.merge(password: '')
+  @visitor = @visitor.merge(:password => "")
   sign_up
 end
 
 When /^I sign up with a to short password$/ do
   create_visitor
-  @visitor = @visitor.merge(password: 'short', password_confirmation: 'short')
+  @visitor = @visitor.merge(:password => "short", :password_confirmation => "short")
   sign_up
 end
 
 When $/ do
   create_visitor
-  @visitor = @visitor.merge(password: '')
+  @visitor = @visitor.merge(:password => "")
   sign_up
 end
 
 
 When /^I sign up with a mismatched password confirmation$/ do
   create_visitor
-  @visitor = @visitor.merge(password_confirmation: 'changeme123')
+  @visitor = @visitor.merge(:password_confirmation => "changeme123")
   sign_up
 end
 
@@ -125,20 +125,20 @@ When /^I return to the site$/ do
 end
 
 When /^I sign in with a wrong email$/ do
-  @visitor = @visitor.merge(email: 'wrong@example.com')
+  @visitor = @visitor.merge(:email => "wrong@example.com")
   sign_in
 end
 
 When /^I sign in with a wrong password$/ do
-  @visitor = @visitor.merge(password: 'wrongpass')
+  @visitor = @visitor.merge(:password => "wrongpass")
   sign_in
 end
 
 When /^I edit my account details$/ do
-  click_link 'Edit account'
-  fill_in 'user_name', with: 'newname'
-  fill_in 'user_current_password', with: @visitor[:password]
-  click_button 'Update'
+  click_link "Edit account"
+  fill_in "user_name", :with => "newname"
+  fill_in "user_current_password", :with => @visitor[:password]
+  click_button "Update"
 end
 
 When /^I look at the list of users$/ do
@@ -147,59 +147,59 @@ end
 
 ### THEN ###
 Then /^I should be signed in$/ do
-  page.should have_content 'Logout'
-  page.should_not have_content 'Sign up'
-  page.should_not have_content 'Login'
+  page.should have_content "Logout"
+  page.should_not have_content "Sign up"
+  page.should_not have_content "Login"
 end
 
 Then /^I should be signed out$/ do
-  page.should have_content 'Sign up'
-  page.should have_content 'Login'
-  page.should_not have_content 'Logout'
+  page.should have_content "Sign up"
+  page.should have_content "Login"
+  page.should_not have_content "Logout"
 end
 
 Then /^I see an unconfirmed account message$/ do
-  page.should have_content 'You have to confirm your account before continuing.'
+  page.should have_content "You have to confirm your account before continuing."
 end
 
 Then /^I see a successful sign in message$/ do
-  page.should have_content 'Signed in successfully.'
+  page.should have_content "Signed in successfully."
 end
 
 Then /^I should see a successful sign up message$/ do
-  page.should have_content 'Welcome! You have signed up successfully.'
+  page.should have_content "Welcome! You have signed up successfully."
 end
 
 Then /^I should see an invalid email message$/ do
-  page.should have_content 'is invalid'
+  page.should have_content "is invalid"
 end
 
 Then /^I should see a missing password message$/ do
-  page.should have_content 'can\'t be blank'
+  page.should have_content "can't be blank"
 end
 
 Then /^I should see a missing password confirmation message$/ do
-  page.should have_content 'doesn\'t match confirmation'
+  page.should have_content "doesn't match confirmation"
 end
 
 Then /^I should see a mismatched password message$/ do
-  page.should have_content 'doesn\'t match confirmation'
+  page.should have_content "doesn't match confirmation"
 end
 
 Then /^I should see a password is too short message$/ do
-  page.should have_content 'is too short'
+  page.should have_content "is too short"
 end
 
 Then /^I should see a signed out message$/ do
-  page.should have_content 'Signed out successfully.'
+  page.should have_content "Signed out successfully."
 end
 
 Then /^I see an invalid login message$/ do
-  page.should have_content 'Invalid email or password.'
+  page.should have_content "Invalid email or password."
 end
 
 Then /^I should see an account edited message$/ do
-  page.should have_content 'You updated your account successfully.'
+  page.should have_content "You updated your account successfully."
 end
 
 Then /^I should see my name$/ do
